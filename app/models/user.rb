2e8d0d939cc8007df11a5873bfc0df5ae3db5083
u-maki <maki0825.w@gmail.com>
2024-10-25 17:06:50 +0900

    ログイン・ログアウト機能実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,11 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :nickname, presence: true
+  validates :birthday, presence: true
+  validates :last_name, presence: true
+  validates :first_name, presence: true
+  validates :last_name_kana, presence: true, format: { with: /\A[ァ-ン]+\z/, message: 'は全角カタカナのみで入力してください。' }
+  validates :first_name_kana, presence: true, format: { with: /\A[ァ-ン]+\z/, message: 'は全角カタカナのみで入力してください。' }
 end
