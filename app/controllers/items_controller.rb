e8a0dbec046fc20db0d7701b0e657a6028bfa1a3
u-maki <maki0825.w@gmail.com>
2024-10-28 19:34:13 +0900

    ActiveHash記述

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,29 @@
 class ItemsController < ApplicationController
-end
+  # ここに before_action を記述
+  before_action :authenticate_user!, only: [:new, :create]
 
-def index
+  def index
+    # 商品一覧ページの表示処理
+  end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path, notice: '商品を出品しました'
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:product_name, :product_description, :category_id, 
+                                 :condition_id, :shipping_cost_id, :prefecture_id, 
+                                 :shipping_time_id, :price, :image).merge(user_id: current_user.id)
+  end
 end
