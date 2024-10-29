205553683bbee6a0cee76112c80149353bee725f
u-maki <maki0825.w@gmail.com>
2024-10-29 13:52:36 +0900

    エラーメッセージ表示確認

--- app/models/item.rb
+++ app/models/item.rb
@@ -8,22 +8,16 @@ class Item < ApplicationRecord
   belongs_to :prefecture, class_name: 'Prefecture'
   belongs_to :shipping_time, class_name: 'ShippingTime'
 
-  has_one_attached :image
-  has_one :purchase_record
-
-  validates :category_id, :condition_id, :shipping_cost_id, :prefecture_id, :shipping_time_id, presence: true
-  validates :product_name, :product_description, :image, presence: true
+  validates :product_name, presence: true
+  validates :product_description, presence: true
+  validates :category_id, presence: true
+  validates :condition_id, presence: true
+  validates :shipping_cost_id, presence: true
+  validates :prefecture_id, presence: true
+  validates :shipping_time_id, presence: true
   validates :price, presence: true,
                     numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 },
                     format: { with: /\A[0-9]+\z/, message: 'は半角数字で入力してください。' }
-
-  # 販売手数料を計算するメソッド
-  def selling_fee
-    (price * 0.1).floor
-  end
-
-  # 販売利益を計算するメソッド
-  def profit
-    (price - selling_fee).floor
-  end
+  has_one_attached :image
+  has_one :purchase_record
 end
