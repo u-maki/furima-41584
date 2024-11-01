911907071ebe03fd1191bf06bc48aeb60e296657
u-maki <maki0825.w@gmail.com>
2024-11-01 09:08:04 +0900

    コントローラー内にindexアクションを記述

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,6 +2,7 @@ class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create]
 
   def index
+    @items = Item.all
   end
 
   def new
@@ -16,6 +17,10 @@ class ItemsController < ApplicationController
       flash.now[:alert] = '入力に不備があります。再度ご確認ください。'
       render :new, status: :unprocessable_entity
     end
+  end
+
+  def show
+    @item = Item.find(params[:id])
   end
 
   private
