8e5561bfbeb14d70647b1a59671e55cf983f368e
u-maki <maki0825.w@gmail.com>
2024-11-02 10:05:56 +0900

    編集機能の実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :items, only: [:new, :create, :index, :show, :edit]
+  resources :items, only: [:new, :create, :index, :show, :edit, :update]
   root to: 'items#index'
 end
 
